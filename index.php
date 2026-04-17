445a62b4c05c482390a784dda8c3d1080f2f9b1d
bidosantos <bidosantos392@gmail.com>
2026-04-17 16:09:31 -0300

    mudei o index

--- index.php
+++ index.php
@@ -1,13 +1,46 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/index.css">
     <title>Livraria Athenas</title>
 </head>
-<body>
-    <h1>Olá mundo!</h1>
 
-    <a href="./pages/login.php">Felinha nas pista</a>
+<body>
+
+    <header>
+
+        <nav>
+            <img src="./assets/img/LOGOS/logo1.png" alt="Logo">
+
+            <ul>
+                <li>Inicio</li>
+                <li>Livros</li>
+                <li>Contato</li>
+                <li>Sobre</li>
+                <a href="./pages/login.php">Login</a>
+            </ul>
+
+        </nav>
+
+    </header>
+
+    <main>
+        <!-- Hero -->
+        <section class="hero">
+
+            <img src="./assets/img/mulherSentada.png" alt="">
+
+            <div class="titulo">
+                <h1>AthenaS</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dignissimos aliquid consequatur possimus eos temporibus expedita fugiat vitae beatae architecto? Temporibus quo dolore officiis natus dolores maiores perspiciatis asperiores doloribus!</p>
+            </div>
+        </section>
+
+    </main>
 </body>
+
 </html>
